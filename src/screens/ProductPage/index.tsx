5c5078d7240bf5486c41c22148dbe512c3a96d1d
perfluka8760 <canalluka8760@gmail.com>
2023-02-01 23:20:47 -0300

    Working in productpage

--- src/screens/ProductPage/index.tsx
+++ src/screens/ProductPage/index.tsx
@@ -1,18 +1,57 @@
 import React from "react";
 import Header from "./components/Header";
-import { BoxImage, Container, Img } from "./styles";
+import {
+  BoxImage,
+  Container,
+  Img,
+  ProductTitle,
+  Rates,
+  RateTitle,
+} from "./styles";
+import StarIcon from "./assets/star.svg";
+import ShareIcon from "./assets/share-icon.svg";
+import HeartIcon from "./assets/heart-icon.svg";
+import Descripion from "../../components/Carrosel/components/Description/Description";
 
 const data = {
-  ImageUrl:
+  title: "Alexa Echodot Amazon 3 Geração Black",
+  ImageUrl1:
     "https://a-static.mlcdn.com.br/450x450/alexa-echodot-amazon-3-geracao-black/oliststore/mglol6o8u7lsirol/57b1490592d84bbe6b9509757b024c35.jpeg",
 };
 
 export default function ProductPage() {
   return (
-    <Container>
+    <>
       <Header />
+      <Container>
+        <Img src={data.ImageUrl1} />
 
-      <Img src={data.ImageUrl} />
-    </Container>
+        <ProductTitle>{data.title}</ProductTitle>
+
+        <Rates>
+          <div
+            style={{
+              marginTop: "0.5rem",
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <div style={{ display: "flex", gap: "0.1rem" }}>
+              <img src={StarIcon} />
+              <img src={StarIcon} />
+              <img src={StarIcon} />
+              <img src={StarIcon} />
+              <img src={StarIcon} />
+            </div>
+            <RateTitle style={{ marginLeft: "0.5rem" }}>5.0</RateTitle>
+            <RateTitle style={{ marginLeft: "0.2rem" }}>(26)</RateTitle>
+            <img src={ShareIcon} />
+          </div>
+          <img src={HeartIcon} />
+        </Rates>
+
+        <Descripion></Descripion>
+      </Container>
+    </>
   );
 }
--- src/screens/ProductPage/styles.ts
+++ src/screens/ProductPage/styles.ts
@@ -2,6 +2,8 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   background-color: #fff;
+  height: 60rem;
+  padding: 0.5rem;
 `;
 
 export const BoxImage = styled.div``;
@@ -14,4 +16,27 @@ export const Img = styled.img`
   max-height: 40vh;
 `;
 
-export const Title = styled.div``;
+export const ProductTitle = styled.div`
+  font-size: 1.5rem;
+  font-family: "Roboto", sans-serif;
+  color: #404040;
+  font-weight: 500;
+`;
+
+export const Rates = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  max-width: 100%;
+`;
+
+export const RateTitle = styled.h1`
+  font-size: 0.8rem;
+  font-family: "Roboto", sans-serif;
+  color: #808080;
+  font-weight: 400;
+`;
+
+export const Description = styled.div`
+  margin-top: 1rem;
+`;
